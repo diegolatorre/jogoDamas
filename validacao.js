a71f72bc66725154eca1530caa3d32f3d2ae94d5
diegolatorre <df.lima98@gmail.com>
2019-09-03 17:46:18 -0300

    Branco fazendo as jogadas obrigatorias em nivel 1

--- validacao.js
+++ validacao.js
@@ -1,4 +1,26 @@
 function validaBranca() {
+
+    if (jogadasObrigatorias.length > 0) {
+        for (let i = 0; i < jogadasObrigatorias.length; i++) {
+            if (jogada.peca == jogadasObrigatorias[i].peca && jogada.casa.c == jogadasObrigatorias[i].destino.c && jogada.casa.l == jogadasObrigatorias[i].destino.l) {
+                superDamaBranca();
+                moverPeca();
+            } else {
+                document.getElementById("jogador").innerHTML = 'Jogadas Obrigatorias a fazer';
+            }
+        }
+    } else {
+        for (let i = 0; i < jogadasPossiveis.length; i++) {
+            if (jogada.peca == jogadasPossiveis[i].peca && jogada.casa.c == jogadasPossiveis[i].destino.c && jogada.casa.l == jogadasPossiveis[i].destino.l) {
+                superDamaBranca();
+                moverPeca();
+            } else {
+                document.getElementById("jogador").innerHTML = 'Jogadas a fazer';
+            }
+        }
+    }
+
+    /*
     var quantidadeCasasLinha = Math.abs(jogada.casa.l - pecas[jogada.peca].casa.l);
     var quantidadeCasasColuna = Math.abs(jogada.casa.c - pecas[jogada.peca].casa.c);
 
@@ -12,7 +34,7 @@ function validaBranca() {
         }
     } else {
         document.getElementById("jogador").innerHTML = 'Jogada nao permitida, tente outra casa';
-    }
+    }*/
 }
 
 function validaPreta() {
